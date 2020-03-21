144ce067c16d1fd1c9b0888e253f74eea7b5760d
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-03-21 11:47:20 -0700

    Tried to add UserForm

--- notescollector/main_app/forms.py
+++ notescollector/main_app/forms.py
@@ -1,7 +1,19 @@
 from django import forms
-from .models import Person
+from .models import Person, Profile
+from django.contrib.auth.models import User
+
 
 class PersonForm(forms.ModelForm):
     class Meta:
         model = Person
         fields = ['name', 'color']
+
+# class UserForm(forms.ModelForm):
+#     class Meta:
+#         model = User
+#         fields = ['username', 'email']
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['location']
