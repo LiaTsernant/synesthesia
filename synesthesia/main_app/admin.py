1754459742f740dd282add839ca39dd7e25ff0b5
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-03-21 16:03:47 -0700

    Added User

--- synesthesia/main_app/admin.py
+++ synesthesia/main_app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Note, Person, Picture
+from django.contrib.auth.models import User
+
 
 # Register all models for being able to see them as ADMIN
 admin.site.register(Note)
